f5b0cb8e6b2009b45d028c70b7e522bea14ee7b7
Ethan He <yfhe@ucdavis.edu>
2021-11-07 12:44:25 -0800

    add test cases

--- main.hs
+++ main.hs
@@ -11,6 +11,14 @@ main = do
   print $ matchStrList ["this", "table", "!"] ["this", "table", "supports", "a", "block"]
   print $ matchStrList ["!", "brown"] ["green", "red", "brown", "yellow"]
   print $ matchStrList ["!", "brown"] ["green", "red", "brown", "brown"]
+  print $ matchStrList ["red", "green", "!", "blue"] ["red", "green", "blue"]
+  print $ matchStrList ["red", "gr*n", "!", "blue"] ["red", "green", "blue"]
+  print $ matchStrList ["t*", "table", "is", "*n"] ["this", "table", "is", "blue"]
+  print $ matchStrList ["color", "apple", "*"] ["color", "apple", "red"]
+  print $ matchStrList ["color", "*", "red"] ["color", "apple", "red"]
+  print $ matchStrList ["color", "*", "red"] ["color", "apple", "green"]
+  print $ matchStrList ["color*red"] ["color", "apple", "red"]
+  print $ matchStrList ["color", "!", "*", "red"] ["color", "apple", "red"]
 
 -- where
 --   xs = ["!", "table", "!"]
