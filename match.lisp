f7b3e624afcb3b96abc466de855bb9cbc2106595
Ethan He <yfhe@ucdavis.edu>
2021-11-08 21:43:39 -0800

    add helper functions for match

--- match.lisp
+++ match.lisp
@@ -1,10 +1,24 @@
 (defun match (pattern match)
-	; This is just to show that it works when there's no quotations
-	(if (string= (first pattern) (first match))
-		t
-		NIL
-	)
-)
+  ; This is just to show that it works when there's no quotations
+  (if (string= (first pattern) (first match))
+      t
+      NIL))
+
+(defun dropwhile (f xs)
+  (if (not (funcall f (car xs)))
+    xs
+    (dropwhile f (cdr xs))))
+
+(defun drop (f xs)
+  (case xs
+    (nil nil)
+    (otherwise
+     (if (not (funcall f (car xs)))
+      (dropwhile (lambda (x) (not (funcall f x))) (cdr xs))
+      (drop f (cdr xs))))))
+
+
+(defun match-base (s f xs ys))
 
 ; You may run the code and the output will be true.
-(print (match '(apple2) '(apple2 banana)))
+(print (drop (lambda (x) (/= x 4)) '(1 2 3 4 4 4 4 4 5 6 7)))
