0d7ec4b23d328800eb98bb2a2681e3ce96be0f17
Ethan He <yfhe@ucdavis.edu>
2021-11-10 17:45:35 -0800

    add lisp match

--- match.lisp
+++ match.lisp
@@ -17,8 +17,29 @@
       (dropwhile (lambda (x) (not (funcall f x))) (cdr xs))
       (drop f (cdr xs))))))
 
+(defun any (f xs)
+  (cond
+    ((null xs) nil)
+    (t (or (funcall f (car xs)) (any f (cdr xs))))))
 
-(defun match-base (s f xs ys))
+(defun match-base (s f xs ys)
+  (cond
+    ((null ys) t)
+    ((null xs) nil)
+    ((eql (cons s nil) xs) t)
+    ((eql s (car xs))
+      (and
+        (any (lambda (x) (funcall f (nth 1 xs) x)) ys) 
+        (match-base s f (cdr (cdr xs)) (drop (lambda (x) (not (funcall f (nth 1 xs) x))) ys))))
+    ((funcall f (car xs) (car ys)) (match-base s f (cdr xs) (cdr ys)))
+    (t nil)))
+
+(defun matchStr (xs ys)
+  (match-base '* #'eql xs ys))
+
+(defun match (xs ys)
+  (match-base '(!) #'matchStr xs ys))
 
 ; You may run the code and the output will be true.
-(print (drop (lambda (x) (/= x 4)) '(1 2 3 4 4 4 4 4 5 6 7)))
+; (print (any (lambda (x) (eq x 's)) '(t h i s)))
+(print (matchStr '(*) '(t h i s)))
