3e319e1b717341c6277dbea29af298abaa4ce6e8
Ethan He <yfhe@ucdavis.edu>
2021-11-17 16:50:20 -0800

    use simplified version of fib

--- fibonacci.lisp
+++ fibonacci.lisp
@@ -4,33 +4,17 @@
         (1 1)
         (otherwise (+ (nth-fib (- n 1)) (nth-fib (- n 2))))))
 
-; list generators
-(defun list-from (n)
-  (case n
-        (0 '(0))
-        (1 '(0))
-        (otherwise (cons (- n 1) (list-from (- n 1))))))
+; append a new fib number to a existing fib list
+(defun append-fib (xs)
+  (nconc xs (list (reduce '+ (last xs 2)))))
 
-;; would be [0..n-1] in Haskell
-(defun list-to (n)
-  (reverse (list-from n)))
-
-; a different version getting nth fib
-(defun nfib (n)
-  (case n
-        (0 0)
-        (1 1)
-        (otherwise
-         (let* ((k (floor n 2))
-                (f1 (nfib k))
-                (f2 (nfib (- k 1))))
-           (cond
-            ((evenp n) (* f1 (+ f1 (* 2 f2))))
-            ((= 1 (mod n 4)) (+ 2 (* (+ (* 2 f1) f2) (- (* 2 f1) f2))))
-            (t (- (* (+ (* 2 f1) f2) (- (* 2 f1) f2)) 2)))))))
+(defun fib-helper (n xs)
+  (if (>= (length xs) n)
+      xs
+      (fib-helper n (append-fib xs))))
 
 (defun fib (n)
-  (mapcar #'nfib (list-to n)))
+  (fib-helper n '(0 1)))
 
 ; helper function
 (defun get-fibs (n xs bound)
